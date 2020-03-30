687e438ae235cfb0762ea6c0a4af6500fb4f7023
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 18:41:16 +0200

    add connect method

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import serveIndex from "serve-index";
+import { sso } from "node-expose-sspi";
+import session from "express-session";
 
 const app = express();
 
+app.use(session({ secret: "this is my secret session..." }));
+
 app.use("/protected/secret", (req, res) => {
   res.json({ hello: "world" });
+});
+
+app.use("/protected/connect", sso.auth(), (req, res) => {
+  if (req.sso) {
+    req.session.sso = req.sso;
+    res.json({ user: req.sso.user });
+    return;
+  }
+  delete req.session.sso;
+  res.status(401).end();
 });
 
 app.use(express.static("."));
