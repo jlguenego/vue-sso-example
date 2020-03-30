9dbddba1682da751202e9c51402d03bddf898924
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 19:02:16 +0200

    give state to component

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  <main>
+  <main v-if="!state.user">
     <h1>Welcome !</h1>
     <p>
       We have a secret...<br />
       and if you want to know it please connect...
     </p>
-    <router-link class="primary" tag="button" to="/login" v-focus>Go to login page ></router-link>
+    <router-link class="primary" tag="button" to="/login" v-focus>
+      Go to login page &gt;
+    </router-link>
+  </main>
+  <main v-else>
+    <h1>Welcome {{ state.user.displayName }}!</h1>
   </main>
 </template>
+
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import store, { User, State } from "../store";
+
+@Component
+export default class Home extends Vue {
+  public state: State = store.state;
+}
+</script>
+
+
 
 <style scoped lang="scss">
 p {
