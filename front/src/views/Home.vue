8b8dfff7efc3e2bbee6475295c62111a3e3cacca
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 16:05:59 +0200

    finishing vue home

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <main>
     <h1>Welcome !</h1>
-    <p>We have a secret, and if you want to know it please connect ;)</p>
+    <p>
+      We have a secret...<br />
+      and if you want to know it please connect...
+    </p>
     <button class="primary">Go to login page ></button>
   </main>
 </template>
+
+<style scoped lang="scss">
+p {
+  text-align: center;
+}
+</style>
 
 
 
