bf691bf258d63990ea698797a777d5c0067912c0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 18:42:30 +0200

    add vue resource

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./directives/focus";
+import VueResource from "vue-resource";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
 
 new Vue({
   router,
