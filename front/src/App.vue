f123f40dbd2fcdf69d10111056c2af37bb50bf3c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 11:18:11 +0200

    upgrade project

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    <h1>{{ message }}</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-export default {
-  name: "App",
-  data: function() {
-    return {
-      message: "Hello World"
-    };
-  }
-};
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
