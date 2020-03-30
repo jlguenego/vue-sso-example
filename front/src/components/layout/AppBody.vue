e03b86b058a883b8309c6063d21f7d3bab0642b3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 12:49:41 +0200

    appBody dom and css

--- front/src/components/layout/AppBody.vue
+++ front/src/components/layout/AppBody.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="app-body">
-    <p>Body works!</p>
+    <main>
+      <h1>Welcome !</h1>
+      <p>We have a secret, and if you want to know it please connect ;)</p>
+      <button>Go to login page ></button>
+    </main>
   </div>
 </template>
 
 <style scoped lang="scss">
 .app-body {
   flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
