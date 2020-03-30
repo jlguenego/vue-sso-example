bba7945e0f1914b800b8f4c5b1e3dc31f7bd786e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 12:36:49 +0200

    footer dom and css

--- front/src/components/layout/AppFooter.vue
+++ front/src/components/layout/AppFooter.vue
@@ -1,3 +1,15 @@
 <template>
-  <p>Footer works!</p>
+  <footer>
+    <a href="#">Legal</a>
+  </footer>
 </template>
+
+<style scoped lang="scss">
+footer {
+  background: #eee;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
