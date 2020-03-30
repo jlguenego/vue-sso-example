4d6783d1da801b4168f90bb948202ea931230829
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-30 17:39:33 +0200

    esInterop + web service

--- back/server.ts
+++ back/server.ts
@@ -1,7 +1,12 @@
-const express = require("express");
-const serveIndex = require("serve-index");
+import express from "express";
+import serveIndex from "serve-index";
 
 const app = express();
+
+app.use("/protected/secret", (req, res) => {
+  res.json({ hello: "world" });
+});
+
 app.use(express.static("."));
 app.use(serveIndex(".", { icons: true }));
 
